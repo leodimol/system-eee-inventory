f642c25a92621c902718c2e59084e7d4fcb1a452
Leo Joy Dimol <eee.ojtleojoydimol@gmail.com>
2026-06-22 13:52:59 +0800

    Add vite config to externalize jspdf and ignore module not found warnings

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,15 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()], server: { host: true, port: 5173, strictPort: true },
+  plugins: [react()],
+  server: { host: true, port: 5173, strictPort: true },
+  build: {
+    rollupOptions: {
+      external: ['jspdf', 'jspdf-autotable'],
+      onwarn(warning, warn) {
+        if (warning.code === 'MODULE_NOT_FOUND') return;
+        warn(warning);
+      }
+    }
+  }
 })
-
